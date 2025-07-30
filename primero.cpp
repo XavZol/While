40c7b1d4cf9ccc11be6813943da5890deff8cae1
XavZol <alansolis707@gmail.com>
2025-07-29 23:51:40 -0600

    Programa while i--

--- primero.cpp
+++ primero.cpp
@@ -2,10 +2,10 @@
 // La sentencia while 
 using namespace std;
 int main(){
-int i = 1;
-while(i <= 10){
+int i = 10; // inicializamos i en 10
+while(i >= 1){ 
     cout<<"El valor de i es:\n"<<i;
-    i++;
+    i--; // decrementamos el valor de i en 1
 }
 
 
