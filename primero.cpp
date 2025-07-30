ef679259e12b8b9ae3d2d859447642d787b43c88
XavZol <alansolis707@gmail.com>
2025-07-30 00:22:41 -0600

    System pause para la libreria de stdlib.h y variable dentro de for

--- primero.cpp
+++ primero.cpp
@@ -3,14 +3,13 @@
 #include<conio.h> // para usar getch()
 using namespace std;
 int main(){
-    // programa con bucle do while
-    int i;
-    i=10;
-    do{
-        cout<<i<<endl;
-        i--; // lo que hace es incrementar i en 1
-    }while(i>=1); // mientras i sea menor o igual a 10 se ejecuta el bucle
-    getch(); // espera a que se presione una tecla antes de cerrar la consola
+    // Sentencia for 
+    // Declaramos la variable dentro de for
+    for(int a=1; a<=10; a++){ 
+        cout<<"El valor de a es: " << a << endl;
+    }
+
+    
 system("pause"); // pausa la consola para ver el resultado
     return 0;   
 
