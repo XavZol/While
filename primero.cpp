61b5bbce415b3b1227bb6a0eafc8deef26ef3679
XavZol <alansolis707@gmail.com>
2025-07-30 01:29:54 -0600

    Programa de leer temperatura cada 4 horas durante 24 horas

--- primero.cpp
+++ primero.cpp
@@ -2,15 +2,26 @@
 #include<conio.h> // para usar getch()
 using namespace std;
 int main(){
-    // Programa de la suma de los cuadrados de los 10 primeros enteros mayores que 10
-    int a = 0, cuadrado;
-    for (int i = 1; i <= 10; i++){
+    // Programa lea la temperatura cada 4 horas por 24 horas y calcule la temperatura media del dia.
+    float temp, mayor=0, menor=999;
+    float total=0; promedio=0;
 
-        cuadrado = i*i;
-        a += cuadrado; // Suma de los cuadrados
+    for( int a=0; a<24, a+=4){
+        cout<<"Digite temperatura de la hora"<<a<<":";
+        cin>>temp;
+        total += temp;
+        if (temp > mayor){
+            mayor = temp;
+        }
+        if (temp < menor){
+            menor = temp;
+        }
+        promedio = total / 6; // 24 horas / 4 horas por lectura = 6 lecturas
 
+        cout<<"Temperatura media hasta la hora "<<a<<": "<<promedio<<endl;
+        cout<<"Temperatura mayor hasta la hora "<<a<<": "<<mayor<<endl;
+        cout<<"Temperatura menor hasta la hora "<<a<<": "<<menor<<endl;
     }
-    cout<<"La suma de los cuadrados de los 10 primeros enteros mayores que 10 es: " << a << endl;
     getch();
     return 0;   
 }
