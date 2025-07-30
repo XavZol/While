6a9f649d5d257099f522de5095e6bda73d15f563
XavZol <alansolis707@gmail.com>
2025-07-30 00:12:35 -0600

    Se agrego la libreria stdlib.h

--- primero.cpp
+++ primero.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
-#include<conio.h>
+#include<stdlib.h>  
+#include<conio.h> // para usar getch()
 using namespace std;
 int main(){
     // programa con bucle do while
     int i;
-    i=1;
+    i=10;
     do{
         cout<<i<<endl;
-        i++; // lo que hace es incrementar i en 1
-    }while(i<=10); // mientras i sea menor o igual a 10 se ejecuta el bucle
+        i--; // lo que hace es incrementar i en 1
+    }while(i>=1); // mientras i sea menor o igual a 10 se ejecuta el bucle
     getch(); // espera a que se presione una tecla antes de cerrar la consola
-    }
-}
+system("pause"); // pausa la consola para ver el resultado
+    return 0;   
+
 }
