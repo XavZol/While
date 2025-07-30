e355ead35d2d897eccde244af99fc4abf154783b
XavZol <alansolis707@gmail.com>
2025-07-30 11:17:09 -0600

    Programa que calcule el valor multiplicado con bucle for

--- primero.cpp
+++ primero.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
-#include<conio.h>
+#include<stdlib.h>
 using namespace std;
 int main(){
-    // Escribe un programa que calcule el valor de: 1+3+5+...+2n-1.
- // solicitar los 10 numeros impares pares e impares por ejemplo.
-    int n, nele,suma = 0;
-    cout<<"Digite un numero para la suma:\n";
-    cin>>nele;
-    fo(int i = 1; 1<=2*nele-1; i++){
-        suma += i;
-    }
-    cout<<"la suma es:\n"<<suma<<endl;
-getch();    
+    // Escriba un programa que calcule el valor de: 1*2*3*4*5*n (factorial)
+    int a, i, resultado = 1;
+    cout<<"Digite un numero para obtener el factorial:\n";
+
+    for(i=1; i<=5; i++){
+        resultado *= i; // Multiplicamos el resultado por i
+    }// siempre para multiplicacion interativa colocar 1 para iniciar.
+    cout<<"El resultado es: "<<resultado<<endl;
+system("pause"); 
     return 0;   
 }
